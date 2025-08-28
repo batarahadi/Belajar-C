b8bef9b882905e64dd6461680cce3de20d72656f
Batara Hadi <67648889+batarahadi@users.noreply.github.com>
2025-08-29 06:42:59 +0900

    update file handling in main.c and remove file.txt content

--- main.c
+++ main.c
@@ -3,11 +3,13 @@
 
 int main() {
     FILE *dataMasuk = fopen("file.txt", "r");
-    FILE *dataKeluar = fopen("file_copy.txt", "w");
+    FILE *dataKeluar = fopen("file.txt", "w");
 
     if (dataMasuk == NULL || dataKeluar == NULL) {
         printf("Error ! File tidak ditemukan.\n");
         exit(-1);
     }
+
+
     return 0;
 }
