e28ea7110ab69e4f210f06e32fb4846a09fcc85d
Batara Hadi <67648889+batarahadi@users.noreply.github.com>
2025-08-29 06:28:37 +0900

    menambahkan beberapa fle

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
+    FILE *dataMasuk = fopen("file.txt", "r");
+    FILE *dataKeluar = fopen("file_copy.txt", "w");
 
-    
+    if (dataMasuk == NULL || dataKeluar == NULL) {
+        printf("Error ! File tidak ditemukan.\n");
+        exit(-1);
+    }
     return 0;
 }
