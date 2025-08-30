2f50a8033fb123c05b021c79d1f43e9f542909da
Batara Hadi <67648889+batarahadi@users.noreply.github.com>
2025-08-30 10:57:48 +0900

    update file.txt menambahkan baris yang dapat menampilkan isi file.txt

--- main.c
+++ main.c
@@ -2,18 +2,32 @@
 #include <stdlib.h>
 
 int main() {
-    FILE *dataMembaca = fopen("file.txt", "r");
     FILE *dataMenulis = fopen("file.txt", "w");
-
-    if (dataMembaca == NULL || dataMenulis == NULL) {
+    if (dataMenulis == NULL) {
         printf("Error ! File tidak ditemukan.\n");
         exit(-1);
     }
 
     char dataMasuk[100];
-    printf("Masukkan data yang akan ditulis ke file: ");
+    printf("Masukkan data yang akan ditulis ke file max 100 karakter: \n");
     scanf("%[^\n]", dataMasuk);
     fprintf(dataMenulis, "%s\n", dataMasuk);
+
+    fclose(dataMenulis);
+
+    FILE *dataMembaca = fopen("file.txt", "r");
+    if (dataMembaca == NULL) {
+        printf("Error ! File tidak ditemukan.\n");
+        exit(-1);
+    }
+
+    char dataKeluar[100];
+    printf("Isi file.tx adalah:\n");
+    while (fgets(dataKeluar, sizeof(dataKeluar), dataMembaca) != NULL) {
+        printf("%s", dataKeluar);
+    }
+    
+    fclose(dataMembaca);
 
     return 0;
 }
