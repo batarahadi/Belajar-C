48cc3cd714ff1a27e065f80f41e59e6a4271f488
Batara Hadi <67648889+batarahadi@users.noreply.github.com>
2025-09-22 20:36:12 +0900

    mengubah semua isi dalam file main.c

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
+int sum(int a, int b) {
+    return a + b;
+}
 int main() {
 
-    
+    int hasil = sum(5, 3);
+    printf("Hasil penjumlahan: %d\n", hasil);
+
     return 0;
 }
