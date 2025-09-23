dbff6fc84b615a02887ed94da2288c9024236aed
Batara Hadi <67648889+batarahadi@users.noreply.github.com>
2025-09-23 11:09:34 +0900

    Menyedehanakan code di file membuatKalkulator.c

--- coba.c
+++ coba.c
@@ -11,10 +11,13 @@
 int main() {
     int data[6] = {12, 45, 7, 34, 23, 56};
     int *ptr = data;
-    int max = *ptr; // Inisialisasi max dengan elemen pertama
+    int max = *ptr;
 
-
+    for (int i = 0; i < 6; i++) {
+        if (*(ptr + i)>max) {
+            max = *(ptr +i);
+        }
+    }
     printf("Nilai maksimum dalam array adalah: %d\n", max);
-
     return 0;
 }
--- membuatKalkulator.c
+++ membuatKalkulator.c
@@ -1,19 +1,9 @@
 #include <stdio.h>
 
-
-int pertambahan(int a, int b) {
-    return a + b;
-}
-int pengurangan(int a, int b) {
-    return a - b;
-}
-int perkalian(int a, int b) {
-    return a * b;
-}
-float pembagian(int a, int b) {
-    return (float)a / b;
-}
-
+int pertambahan(int a, int b) {return a + b;}
+int pengurangan(int a, int b) {return a - b;}
+int perkalian(int a, int b) {return a * b;}
+float pembagian(int a, int b) {return (float)a / b;}
 
 int main() {
     int data[2];
