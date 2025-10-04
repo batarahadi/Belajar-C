63fc2d9e410237cfc5900eec9aabded9b70d841c
Batara Hadi <67648889+batarahadi@users.noreply.github.com>
2025-10-04 10:34:33 +0900

    membuat file typedef

--- deffinition/struct.c
+++ deffinition/struct.c
@@ -1,27 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 
-
-struct Maha {
-    int id;
-    char nama[20];
-    float nilai;
-};
-
-int main() {
-    struct Maha mh;
-
-    mh.id = 5301;
-    strcpy(mh.nama, "batara");
-    mh.nilai = 85.9;
-
-    printf("ID : %d\n", mh.id );
-    printf("Nama: %s\n", mh.nama );
-    printf("Nilai: %.1f\n", mh.nilai );
-
-    return 0;
-}
-
 struct tmhs {
     char nim[11];
     char name[26];
