14924c478c0b6c743a626f2acea66c3e7521b080
Batara Hadi <67648889+batarahadi@users.noreply.github.com>
2025-10-06 21:17:56 +0900

    berlatih

--- coba.py
+++ coba.py
@@ -9,3 +9,10 @@ for i in range(n):
             arr[j], arr[j+1] = arr[j+1], arr[j]  # swap
 
 print(f"Sorted array: {arr}")
+
+dataBaru = sorted(arr, reverse=True)
+print(f"Sorted array in descending order using sorted(): {dataBaru}")
+
+
+dataBaru.sort()
+print(f"Sorted array using built-in sort: {dataBaru}")
