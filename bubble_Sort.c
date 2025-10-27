4c327a0bb9432b582aab79fc407331f02c61e9e0
Batara Hadi <67648889+batarahadi@users.noreply.github.com>
2025-10-27 22:53:18 +0900

    menambah file buble sort

---
+++ bubble_Sort.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+// Definisi fungsi bubble sort
+void bubbleSort(int arr[], int n) {
+    int i, j, temp;
+
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int arr[5] = {5, 3, 4, 1, 2};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    bubbleSort(arr, n); // Pemanggilan fungsi
+
+    printf("Hasil pengurutan: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
--- while.c
+++ while.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
-int arr[] = {64, 34, 25, 12, 22, 11, 90};
-
 int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
     int i = 0;
     while (i < sizeof(arr) / sizeof(arr[0])) {
         printf("%d ", arr[i]);
