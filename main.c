6d00945681e8f0a5d8b7a7f2955faaffaff4a0c6
Batara Hadi <67648889+batarahadi@users.noreply.github.com>
2025-10-27 22:09:14 +0900

    manambah file

--- main.c
+++ main.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 
-int main() {
-    int lb = 0, ub = 9;
-
-    while (lb<ub) {
-        printf("1");
+int linear_search(int arr[], int n, int key) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == key)
+            return i;
     }
-    
-    
+    return -1;  // tidak ditemukan
+}
+
+int main() {
+    int arr[] = {10, 23, 5, 46, 17};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int key = 41;
+    int idx = linear_search(arr, n, key);
+    if (idx != -1)
+        printf("Elemen %d ditemukan di indeks %d\n", key, idx);
+    else
+        printf("Elemen %d tidak ditemukan\n", key);
     return 0;
 }
