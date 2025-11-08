62df8856c5f7fedc4bb6151c90ec52191dc0ee46
Batara Hadi <67648889+batarahadi@users.noreply.github.com>
2025-11-09 07:54:43 +0900

    p

--- main.c
+++ main.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+static int a = 5;
+static void apalah() {
+    static b = 10;
+    printf("Nilai a di dalam apalah(): %d\n", b);
+}
+
+
 int main() {
-    int *p = malloc(sizeof(int)); // Alokasi untuk 1 integer
-
-    printf("\nIsi array: ");
-    for (int i = 0; i < 5; i++) {
-        printf("%d ", p[i]);
-    }
-
-    free(p); // Bebaskan memori
+    printf("Nilai a di dalam main(): %d\n", a);
+    apalah();
+    a += 10;
+    printf("Nilai a setelah diubah di main(): %d\n", a);
 
     return 0;
 }
